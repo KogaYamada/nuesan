bcca825b0fe11229c66f399d3fe6a895fff223cf
naopin <naopin708@yahoo.co.jp>
2020-06-14 15:52:05 +0900

    app

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// import firebase from 'firebase/app'
+// import 'firebase/auth'
+// import'firebase/database'
+import firebase from "firebase";
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC9tX52m5ama2qD3pBEP0S_NGZ0ogDKKxE",
+  authDomain: "givemoneyapp.firebaseapp.com",
+  databaseURL: "https://givemoneyapp.firebaseio.com",
+  projectId: "givemoneyapp",
+  storageBucket: "givemoneyapp.appspot.com",
+  messagingSenderId: "554202918248",
+  appId: "1:554202918248:web:8fe85816228caf5438badd",
+  measurementId: "G-KHTNRS4Z2Z"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+// const admin = require('firebase-admin');
+// const functions = require('firebase-functions');
+// admin.initializeApp(functions.config().firebase);
+
+
+export const firebaseDB = firebase.database()
+
---
+++ src/views/Register.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <h1>新規登録画面</h1>
+    <table>
+      <tr>
+        <td>
+          <label class="label" for="username">UserName</label>
+        </td>
+        <td>
+          <input id="username" type="text" v-model="username" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label for="email">E-mail</label>
+        </td>
+        <td>
+          <input id="email" type="email" v-model="email" />
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <label for="password">Password</label>
+        </td>
+        <td>
+          <input id="password" type="password" v-model="password" />
+        </td>
+      </tr>
+    </table>
+    <br />
+    <br />
+
+    <button @click="register">新規登録</button>
+    <br />
+    <router-link to="/login">ログインはこちらから</router-link>
+  </div>
+</template>
+
+<style>
+table {
+  margin: 0 auto;
+}
+button {
+  padding: 8px 15px;
+  border-radius: 5px;
+}
+</style>
+
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    register: function() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          
+          firebase
+            .auth()
+            .currentUser.updateProfile({ displayName: this.username });
+
+          const userdata = firebase.auth().currentUser;
+          firebase()
+            .firestore()
+            .collection('myApp')
+            .doc('work')
+            .set({
+              id: userdata.uid,
+              name: userdata.displayName
+            })
+          
+          this.username = "";
+          this.email = "";
+          this.password = "";
+          this.$router.push("/");
+
+        })
+        .catch(error => {
+          alert(error.message);
+        });
+    }
+   
+  }
+  
+};
+</script>
